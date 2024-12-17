5211c98f0f58f58c0bf3f906d83a39723579b76b
WillianDDaniel <williandeivitidaniel@live.com>
2024-12-16 23:56:46 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script>
+  import Nav from './components/Nav.vue'
+  export default {
+    name: 'App',
+    components: {
+      Nav
+    }
+  }
+</script>
+
+<template>
+  <v-app>
+    <Nav />
+  </v-app>
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/Nav.vue
@@ -0,0 +1,24 @@
+<script>
+  import narutoIcon from '../assets/naruto.png'
+  export default {
+    data() {
+      return {
+        imageUrl: narutoIcon
+      }
+    }
+  }
+</script>
+
+<template>
+  <v-toolbar density="comfortable" :elevation="1" color="grey-darken-4">
+    <v-img :src="imageUrl" class="ml-4" contain width="40" height="40" style="max-width: 40px; max-height: 40px;"></v-img>
+    <v-toolbar-title class="font-weight-bold font-italic">Naruto Universe</v-toolbar-title>
+    <v-app-bar-nav-icon></v-app-bar-nav-icon>
+  </v-toolbar>
+</template>
+
+<style scoped>
+  .v-img {
+    margin-right: -6px;
+  }
+</style>
