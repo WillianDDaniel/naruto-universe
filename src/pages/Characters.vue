88aae8bd0d80fe98ccb3a107c89d8d906ae5062f
WillianDDaniel <williandeivitidaniel@live.com>
2024-12-24 01:50:04 -0300

    feat: added skeleton on characters page

--- src/pages/Characters.vue
+++ src/pages/Characters.vue
@@ -13,7 +13,7 @@ export default {
   data() {
     return {
       characters: [],
-      filteredCharacters: [],
+      filteredCharacters: null,
       search: ''
     }
   },
@@ -34,6 +34,7 @@ export default {
   async mounted() {
     await this.getCharacters();
     this.filterCharacters();
+
   }
 }
 </script>
@@ -43,20 +44,26 @@ export default {
   <main>
     <v-container>
       <h1 class="text-h3 mb-6 text-center">Personagens</h1>
-      <v-text-field
-        v-model="search"
-        label="Pesquisar"
-        placeholder="Digite o nome do personagem"
-        dense
-        @input="filterCharacters"
-      ></v-text-field>
+
+      <v-text-field v-model="search" label="Pesquisar" placeholder="Digite o nome do personagem"
+        dense outlined @input="filterCharacters"
+      >
+      </v-text-field>
+
       <v-row>
+        <v-col v-if="!filteredCharacters" v-for="n in 12" :key="n"
+          cols="12" sm="6" md="4" lg="3"
+        >
+          <v-skeleton-loader :elevation="1" type="image, article"></v-skeleton-loader>
+        </v-col>
+
         <v-col v-for="character in filteredCharacters" :key="character.id"
           cols="12" sm="6" md="4" lg="3"
         >
           <characterCard :character="character" />
         </v-col>
       </v-row>
+
     </v-container>
   </main>
   <Footer />
